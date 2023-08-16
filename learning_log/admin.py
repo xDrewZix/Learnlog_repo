433917fcf0b2639b110c9b371b7bdf7bcf7d6046
Drews_World_3 <andrewjfcrawshaw@hotmail.co.uk>
2023-08-16 19:07:34 +0100

    defining urls for web pages

--- learning_log/admin.py
+++ learning_log/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Topic
+from .models import Topic, Entry
 
 admin.site.register(Topic)
+admin.site.register(Entry)
 
