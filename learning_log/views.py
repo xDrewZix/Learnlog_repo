e0c792c03fb12c57a3aee5d7880a8c055802d9af
Drews_World_3 <andrewjfcrawshaw@hotmail.co.uk>
2023-08-17 21:31:45 +0100

    Added topic entries and links

--- learning_log/views.py
+++ learning_log/views.py
@@ -14,4 +14,11 @@ def topics(request):
 	context = {'topics': topics}
 	return render(request, 'learning_log/topics.html', context)
 	
+def topic(request, topic_id):
+	#Show a single topic and all its entries
+	topic = Topic.objects.get(id=topic_id)
+	entries = topic.entry_set.order_by('-date_added')
+	context = {'topic': topic, 'entries': entries}
+	return render(request, 'learning_log/topic.html', context)
+	
 	
